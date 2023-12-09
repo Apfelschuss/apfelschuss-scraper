38717163ae1f2af91a602034fbc945ee37441c40
philippbruhin <info@philippbruhin.ch>
2023-12-09 16:06:55 +0100

    add description to the project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# apfelschuss-scraper
+# Apfelschuss.io Scraping Bot integration
+
+This project grabs likes, followers and posts from different social media sites with help of [scrapingbot](https://www.scraping-bot.io/dashboard/).
+
+# Run locally
+
+1. Clone this repo with `git clone https://github.com/Apfelschuss/apfelschuss-scraper`
+
+2. This project is using [Node.js](https://nodejs.org/en) in the version `v18.14.2`. If you have nvm installed on your machine, just run `nvm install` and you are good to go.
+
+3. Cd into the repo and run `pnpm install`.
+
+4. Copy the `.env.example` file, rename it to `.env` and add your credentials from [scraping-bot.io/dashboard](https://www.scraping-bot.io/dashboard/)
+
+5. Build the JavaScript files with `pnpm run build`.
+
+6. Run the project with `node ./dist/index.js`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,61 @@
-console.log('Hello World!');
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface ResponseData {
+  responseId: string;
+}
+
+const username = process.env.SCRAPINGBOT_USERNAME;
+const apiKey = process.env.SCRAPINGBOT_API_KEY;
+const apiEndPoint = 'http://api.scraping-bot.io/scrape/data-scraper';
+const auth = 'Basic ' + Buffer.from(username + ':' + apiKey).toString('base64');
+
+async function scrape() {
+  try {
+    const response = await axios.post(
+      apiEndPoint,
+      {
+        scraper: 'linkedinCompanyProfile',
+        url: 'https://linkedin.com/company/google',
+      },
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: auth,
+        },
+      },
+    );
+    console.log(JSON.stringify(response.data, null, 2));
+
+    const data = response.data as ResponseData;
+
+    const responseUrl =
+      'http://api.scraping-bot.io/scrape/data-scraper-response?scraper=linkedinCompanyProfile&responseId=' +
+      data.responseId;
+    const finalResponse = await axios.get(responseUrl, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: auth,
+      },
+    });
+    return finalResponse.data as string;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+scrape()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
