e2d787fda2a2df80ce6f2ef69279f3cd456e1c12
philippbruhin <info@philippbruhin.ch>
2023-12-10 19:45:16 +0100

    add timeout and while loop for several tries

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,21 @@ interface ResponseData {
   responseId: string;
 }
 
+interface FinalResponseData {
+  status: string;
+  message: string;
+}
+
 const username = process.env.SCRAPINGBOT_USERNAME;
 const apiKey = process.env.SCRAPINGBOT_API_KEY;
 const apiEndPoint = 'http://api.scraping-bot.io/scrape/data-scraper';
 const auth = 'Basic ' + Buffer.from(username + ':' + apiKey).toString('base64');
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 async function scrape() {
   try {
@@ -18,7 +29,7 @@ async function scrape() {
       apiEndPoint,
       {
         scraper: 'linkedinCompanyProfile',
-        url: 'https://linkedin.com/company/google',
+        url: 'https://linkedin.com/company/sintio',
       },
       {
         headers: {
@@ -29,18 +40,27 @@ async function scrape() {
     );
     console.log(JSON.stringify(response.data, null, 2));
 
+    let finalData: FinalResponseData | null;
+    let loopCount: number = 0;
+
     const data = response.data as ResponseData;
 
-    const responseUrl =
-      'http://api.scraping-bot.io/scrape/data-scraper-response?scraper=linkedinCompanyProfile&responseId=' +
-      data.responseId;
-    const finalResponse = await axios.get(responseUrl, {
-      headers: {
-        Accept: 'application/json',
-        Authorization: auth,
-      },
-    });
-    return finalResponse.data as string;
+    do {
+      await sleep(1000);
+      const responseUrl =
+        'http://api.scraping-bot.io/scrape/data-scraper-response?scraper=linkedinCompanyProfile&responseId=' +
+        data.responseId;
+      const finalResponse = await axios.get(responseUrl, {
+        headers: {
+          Accept: 'application/json',
+          Authorization: auth,
+        },
+      });
+      loopCount++;
+      finalData = finalResponse.data as FinalResponseData;
+    } while (finalData == null || finalData.status === 'pending');
+
+    return { finalData, loopCount };
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.log('error message: ', error.message);
@@ -54,7 +74,12 @@ async function scrape() {
 
 scrape()
   .then((result) => {
-    console.log(result);
+    if (typeof result === 'string') {
+      console.log('Error:', result);
+    } else {
+      console.log('Tries:', result.loopCount.toString());
+      console.log('Final Data:', result.finalData);
+    }
   })
   .catch((error) => {
     console.error(error);
